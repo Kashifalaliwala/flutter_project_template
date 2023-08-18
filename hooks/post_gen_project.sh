8910767ecec53b8f48519dd6a1d51b7cc6d93e90
Kashifa Laliwala <kashifalaliwala@gmail.com>
2023-08-18 15:34:01 +0530

    feat: Set Launcher Icons

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -61,14 +61,15 @@ update_project_permissions
 fetch_splash_image
 flutter pub get
 flutter pub global activate flutter_gen
+flutter pub run flutter_launcher_icons
 dart run build_runner build --delete-conflicting-outputs
 cd ios/
 pod install
 cd ..
 dart format lib
 initialize_git
-{%- if cookiecutter.repo_link != "NA" -%}
+{ %- if cookiecutter.repo_link != "NA" -% }
 connect_git_repo
-{% endif %}
+{ % endif % }
 git config core.hooksPath .githooks/
 attempt_to_launch_studio
